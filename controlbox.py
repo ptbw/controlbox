e729252b4fbd2fb8606fbf1530c5a15652da47d1
Phil <ptbwillis@gmail.com>
2018-07-13 16:14:58 +0100

    Initial commit

---
+++ controlbox.py
@@ -0,0 +1,34 @@
+import web
+ 
+def make_text(string):
+    return string
+ 
+urls = ('/', 'dashboard')
+render = web.template.render('templates/')
+ 
+app = web.application(urls, globals())
+ 
+my_form = web.form.Form(
+                web.form.Textbox('', class_='textfield', id='textfield'),
+                )
+ 
+class dashboard:
+    def GET(self):
+        form = my_form()
+        return render.dashboard(form, "0")
+         
+    def POST(self):
+		form = my_form()
+		form.validates()
+		greenBtn = form.value['green']
+		print( "Green Button: " + greenBtn )
+		s = form.value['textfield']
+		i = 0
+		if( s.isdigit() ) :
+			i = int(s) * 1000
+		
+		return make_text(i)
+ 
+if __name__ == '__main__':
+    app.run()
+
